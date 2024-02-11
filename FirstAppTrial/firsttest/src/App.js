9c03aa5b205198f853572c09a37a5fdd876dfa84
Mahmoud El-Sayed <me974931@gmail.com>
2024-02-11 14:07:59 +0200

    train

---
+++ FirstAppTrial/firsttest/src/App.js
@@ -0,0 +1,16 @@
+import Footer from "./Footer"
+import Header from "./Header"
+import { Button } from "react-bootstrap";
+const App=()=>{
+  return(<>
+  <h1 className="class1">my website</h1>
+  <Button>clickme</Button>
+    <Header></Header>
+    <div>  <br></br><br></br><br></br><br></br><br></br><br></br><br></br></div>
+    <Footer></Footer>
+  </>)
+    
+  
+  }
+
+  export default App
---
+++ FirstAppTrial/firsttest/src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './css/headercss.css'
+const Header = () => {
+  return (
+    <h1 className='headercss'>
+      Header
+    </h1>
+  )
+}
+
+export default Header
